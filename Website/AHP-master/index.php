4141b07e2cf99b3aa9712bba83ddffffe948cce5
aderastafara32 <aderastafara32@gmail.com>
2022-01-09 16:20:19 +0700

    first commit

---
+++ Website/AHP-master/index.php
@@ -0,0 +1,75 @@
+
+<?php
+include('config.php');
+include('fungsi.php');
+
+// header
+include('header.php');
+?>
+
+	<section class="content">
+			<h2 class="ui header">Analitycal Hierarchy Process (AHP)</h2>
+
+			<p>Analitycal Hierarchy Process (AHP) Adalah metode untuk memecahkan suatu situasi yang
+komplek tidak terstruktur kedalam beberapa komponen dalam susunan yang hirarki, dengan memberi
+nilai subjektif tentang pentingnya setiap variabel secara relatif, dan menetapkan variabel mana yang
+memiliki prioritas paling tinggi guna mempengaruhi hasil pada situasi tersebut. </p>
+			<ol class="ui list">
+				<li>Struktur yang berhirarki, sebagai konsekuesi dari kriteria yang  dipilih, sampai pada subkriteria yang paling dalam.</li>
+				<li>Memperhitungkan validitas sampai dengan batas toleransi inkonsistensi berbagai kriteria dan alternatif yang dipilih oleh pengambil keputusan.</li>
+				<li>Memperhitungkan daya tahan output analisis sensitivitas pengambilan keputusan.</li>
+			</ol>
+
+			<br>
+
+			<h3 class="ui header">Tabel Tingkat Kepentingan menurut Saaty (1980)</h3>
+			<table class="ui collapsing striped blue table">
+				<thead>
+					<tr>
+						<th>Nilai Numerik</th>
+						<th>Tingkat Kepentingan <em>(Preference)</em></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class="center aligned">1</td>
+						<td>Sama pentingnya <em>(Equal Importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">2</td>
+						<td>Sama hingga sedikit lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">3</td>
+						<td>Sedikit lebih penting <em>(Slightly more importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">4</td>
+						<td>Sedikit lebih hingga jelas lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">5</td>
+						<td>Jelas lebih penting <em>(Materially more importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">6</td>
+						<td>Jelas hingga sangat jelas lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">7</td>
+						<td>Sangat jelas lebih penting <em>(Significantly more importance)</em></td>
+					</tr>
+					<tr>
+						<td class="center aligned">8</td>
+						<td>Sangat jelas hingga mutlak lebih penting</td>
+					</tr>
+					<tr>
+						<td class="center aligned">9</td>
+						<td>Mutlak lebih penting <em>(Absolutely more importance)</em></td>
+					</tr>
+				</tbody>
+			</table>
+
+	</section>
+
+<?php include('footer.php'); ?>
